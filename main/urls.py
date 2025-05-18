46a524c426d328ca1824afff789cf5aaea296ca5
Dima Holovin <dimaholovin@gmail.com>
2025-05-18 13:24:51 +0300

    Finished!

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.start, name='start'),            # сторінка розстановки кораблів
-    path('save_ships/', views.save_ships, name='save_ships'),  # збереження розстановки
-    path('play/', views.play, name='play'),         # ігровий екран
-    path('shot/', views.shot, name='shot'),         # постріл гравця
-    path('bot_shot/', views.bot_shot, name='bot_shot'),  # постріл бота
+    path('', views.start, name='start'),
+    path('save_ships/', views.save_ships, name='save_ships'),
+    path('play/', views.play, name='play'),
+    path('shot/', views.shot, name='shot'),
+    path('bot_shot/', views.bot_shot, name='bot_shot'),
 ]
